62120bcd009788e399e4701f4e9711b7f511cd65
diego.dopazo.garcia <diego.dopazo.garcia@gmail.com>
2024-06-12 19:01:05 +0200

    Hecha la pagina de favoritos de forma completa, falta ponerla bonita

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+"use client"
+import Loader from "@/components/Loader"
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <div className="flex flex-row items-center"><Loader/></div>
+} 
---
+++ src/components/FavouriteCard.tsx
@@ -0,0 +1,50 @@
+import { deleteFavorite } from "@/lib/fetchUtils";
+import { Session } from "@supabase/supabase-js";
+import { useState } from "react";
+import Loader from "./Loader";
+
+interface FavouriteCardProps {
+    joke: UserJokes;
+    session: Session | null;
+    onRemove: (jokeId: string) => void;
+}
+export default function FavourtiteCard({joke, session, onRemove} : FavouriteCardProps){
+
+    const [isDataLoading, setIsDataLoading] = useState(false);
+
+    async function removeFromFavourite(){
+        setIsDataLoading(true);
+        const jokee: ChuckNorrisJoke = {
+            categories: [], // Asigna categorías según sea necesario
+            created_at: '', // Asigna la fecha de creación
+            icon_url: '', // Asigna la URL del icono
+            id: joke.joke_id,
+            updated_at: '', // Asigna la fecha de actualización
+            url: '', // Asigna la URL del chiste
+            value: joke.joke,
+          };
+        const error = await deleteFavorite(session?.user.id, jokee);
+        setIsDataLoading(false);
+
+        if (!error) {
+            onRemove(joke.joke_id);
+        }else{
+            alert("Error while deleting favourite: " + error.message);
+        }
+      }
+
+    return(
+        <div className="bg-teal-350 my-5 p-5 font-semibold text-2xl flex flex-row items-center">
+            <br/>
+            <div className="ml-10 mr-auto">
+            {joke.joke}
+            </div>
+            <div>
+                {isDataLoading ? <Loader/> : <button onClick={removeFromFavourite} className="bg-red-400 hover:bg-red-600 text-white justify-end mr-10 p-4 rounded">
+                    Del
+                </button>}
+            </div>
+            <br />
+        </div>
+    )
+}
